170e98cf8f884420c01c8c2ef3193bb96d9f1b10
Viral Ruparel <viral@sysbioz.com>
2020-02-11 21:34:44 +0530

    Scaffolding

---
+++ client/src/components/app/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../assets/css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          WeConnect
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
