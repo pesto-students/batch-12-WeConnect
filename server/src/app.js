42bb47cef67464c818ec42cadc05f0d2ba9bfd70
Vineet Srivastav <vineetvk01@gmail.com>
2020-02-12 23:48:21 +0530

    jest-init

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./app/routes/index');
+var indexRouter = require('./routes/index');
 
 var app = express();
 
