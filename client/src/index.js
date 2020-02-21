26d65ca62b6519f802dd5b0db8ba18fa9a47f849
ColdFire <fanilgandhi@gmail.com>
2020-02-21 17:00:54 +0530

    Add Login View

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app/App';
 import { MuiThemeProvider } from '@material-ui/core';
+import { StylesProvider } from '@material-ui/core/styles';
 import theme from './components/Theme/theme';
 
 ReactDOM.render(
   <MuiThemeProvider theme={theme}>
-    <App />
+    <StylesProvider injectFirst>
+      <App />
+    </StylesProvider>
   </MuiThemeProvider>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
