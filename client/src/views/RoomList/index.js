c971255169f330c299605c376546dacd001de433
Nitish Mittal <nitishmittal1990@gmail.com>
2020-02-27 19:17:57 +0530

    Slider Done

--- client/src/views/RoomList/index.js
+++ client/src/views/RoomList/index.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import LinearProgress from '../../components/Generic/ProgressBar/LinearProgress';
 import getRoomData from '../../apis/getRooms';
-import { Container, Grid } from '../../components/Generic';
+import { Container, Grid, TextField } from '../../components/Generic';
 import Room from '../../containers/room';
+import moment from 'moment';
 
 import style from './RoomList.module.css';
 
 const RoomList = (props) => {
-  console.log('roomliong', props);
   const [floors, setFloors] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [date, setDate] = useState(new moment().format('YYYY-MM-DD'));
+
   useEffect(() => {
     getRoomData(props.location.pathname).then((floorsData) => {
       console.log(floorsData);
@@ -18,16 +20,34 @@ const RoomList = (props) => {
     });
   }, [props.location.pathname]);
 
+  const updateDate = (e) => {
+    setDate(e.target.value);
+  };
+
   return (
     <section>
       {isLoading ? <LinearProgress /> : ''}
       <Container fixed>
+        <TextField
+          id="date"
+          label="Date"
+          type="date"
+          defaultValue={date}
+          onChange={updateDate}
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
         {floors.map((eachFloor) => {
           if (eachFloor.rooms.length < 1) {
             return null;
           }
           return (
-            <section className={style.floorSection} key={eachFloor._id}>
+            <section
+              className={style.floorSection}
+              key={eachFloor._id}
+              id={eachFloor._id}
+            >
               <Grid container spacing={4}>
                 <Grid item xs={12}>
                   <p className={style.floorName}>{eachFloor.name}</p>
@@ -37,7 +57,17 @@ const RoomList = (props) => {
                     <Grid item xs={12} md={4}>
                       <Room
                         rooms={room}
-                        operationHours={props.history.location.state}
+                        day={moment(date).day()}
+                        operationHours={
+                          props.history.location.state.operationHours
+                        }
+                        floorId={eachFloor._id}
+                        floorName={eachFloor.name}
+                        //                 ownerId: workspace.owner,
+                        // locationName: workspace.locationName,
+                        // locationId:workspace.locationId,
+                        // workspaceName: workspace.name,
+                        // workspaceId: workspace._id
                       />
                     </Grid>
                   );
